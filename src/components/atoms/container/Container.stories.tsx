78b37fb490b74fc2b160e744707e5f0e345d65ae
Mark_Rumjantsev <mrk.ru@list.ru>
2023-04-05 18:53:05 +0300

    add components: Card, Container

---
+++ src/components/atoms/container/Container.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Container } from './Container';
+
+export default {
+  title: 'Example/Container',
+  component: Container,
+  
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    sizeContainer: {
+      options: ['1024px', '1100px', '1200px', '1440px'],
+      control: 'select'
+    }
+  },
+} as ComponentMeta<typeof Container>;
+
+const Template: ComponentStory<typeof Container> = (args) => <Container {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
